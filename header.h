ec51963a4d65ca673760467cba476e3d4d315d21
benapier <napier.ben.e@gmail.com>
2019-01-28 13:16:04 +0000

    Initial commit

---
+++ header.h
@@ -0,0 +1,10 @@
+//
+// Created by Ben Napier on 2019-01-28.
+//
+
+#ifndef ADS_IMPLEMENTATIONS_HEADER_H
+#define ADS_IMPLEMENTATIONS_HEADER_H
+
+void BucketSort(int array[], int array_size);
+
+#endif //ADS_IMPLEMENTATIONS_HEADER_H
---
+++ sorting/BucketSort.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include "../header.h"
+
+void BucketSort(int data_array[], int array_size) {
+    // First we find the maximum value in the list so we know how much space to allocate.
+    int maximum_value = 0;
+    for (int i = 0; i < array_size; ++i) {
+        if (data_array[i] > maximum_value) {
+            maximum_value = data_array[i];
+        }
+    }
+    auto bucket_vector = std::vector<unsigned >(static_cast<unsigned int>(maximum_value + 1));
+    // And now we fill our buckets.
+    for (int i = 0; i < array_size; ++i) {
+        bucket_vector[data_array[i]]++;
+    }
+    // Now we convert back to our array.
+    int count = 0;
+    for (int i = 0; i < bucket_vector.capacity(); ++i) {
+        for (int j = 0; j < bucket_vector[i]; ++j) {
+            data_array[count] = i;
+            count++;
+        }
+    }
+}
